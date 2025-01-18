096abd5abb198dd1ad897e6524e708a376ed3366
Maria Iklevich <mashavalatkevich@mail.ru>
2025-01-18 21:59:53 +0300

    lesson_2

---
+++ test/src/test/java/L6/Cat.java
@@ -0,0 +1,31 @@
+package L6;
+
+class Cat extends Animal {
+    String name;
+    static int countCat = 0;
+        public Cat(){
+            countCat++;
+        }
+        public Cat(String name) {
+            this.name = name;
+            countCat++;
+        }
+
+        public void runCat(int x) {
+        if (x <= 200) {
+            System.out.println(name+" пробежал "+x+" м.");
+        } else if (x > 200) {
+            System.out.println(name+"не бегает столько");
+        } else {
+            System.out.println("Ошибка значения");
+        }
+
+    }
+        public void swimCat(int x) {
+            System.out.println(name+ " не умеет плавать");
+        }
+
+//        public void catInfo() {
+//            System.out.println("Кот " + name + " " + swim + " и " + run);
+//        }
+    }
