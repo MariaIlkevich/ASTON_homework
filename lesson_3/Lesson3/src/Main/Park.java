d04dea7814d7882cc60e850aef34c68cbc934797
Maria Iklevich <mashavalatkevich@mail.ru>
2025-02-02 13:01:28 +0300

    Lesson3_2.1

--- lesson_3/Lesson3/src/Main/Park.java
+++ lesson_3/Lesson3/src/Main/Park.java
@@ -2,13 +2,13 @@ package Main;
 
 public class Park {
 
-	public class Attraction{
+	public class Attraction {
 		private String name;
 		private String time;
 		private int price;
-		
-		public Attraction (String name, String time, int price) {
-			this.name  = name;
+
+		public Attraction(String name, String time, int price) {
+			this.name = name;
 			this.time = time;
 			this.price = price;
 		}
--- lesson_3/Lesson3/src/Main/Personal.java
+++ lesson_3/Lesson3/src/Main/Personal.java
@@ -7,17 +7,18 @@ public class Personal {
 	private String phone;
 	private int salary;
 	private int age;
-	
-	public Personal (String fullName,String position, String email, String phone, int salary, int age) {
+
+	public Personal(String fullName, String position, String email, String phone, int salary, int age) {
 		this.fullName = fullName;
 		this.position = position;
 		this.email = email;
 		this.phone = phone;
 		this.salary = salary;
 		this.age = age;
-				}
+	}
 
-	public void fullInfo () {
-		System.out.println("\nФИО: " + fullName + "\nДолжность: " + position + "\nEmail: " + email +"\nТелефон: " + phone + "\nЗарплата: " + salary + "\nВозраст: " + age);	
-		}
+	public void fullInfo() {
+		System.out.println("\nФИО: " + fullName + "\nДолжность: " + position + "\nEmail: " + email + "\nТелефон: "
+				+ phone + "\nЗарплата: " + salary + "\nВозраст: " + age);
+	}
 }
