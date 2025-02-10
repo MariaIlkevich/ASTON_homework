13153ef0283571ef89258fee5e5c9bfd2fc9b7ed
Maria Iklevich <mashavalatkevich@mail.ru>
2025-02-10 16:35:53 +0300

    первая загрузка

---
+++ lesson_12/src/lesson_12_testing/lesson_12_testing.java
@@ -0,0 +1,19 @@
+package lesson_12_testing;
+
+public class lesson_12_testing {
+
+	public static int getFactorial(int f) {
+		int result = 1;
+		if (f == 1 || f == 0) {
+			return result = 1;
+		} else if (f < 0) {
+			System.out.println("Введите положительное число");
+			return result = 0;
+		} else {
+			for (int i = 1; i <= f; i++) {
+				result = result * i;
+			}
+			return result;
+		}
+	}
+}
---
+++ lesson_12/src/lesson_12_testing/main.java
@@ -0,0 +1,11 @@
+package lesson_12_testing;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(lesson_12_testing.getFactorial(4));
+
+	}
+}
